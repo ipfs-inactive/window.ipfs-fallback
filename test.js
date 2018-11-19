82ba3d6c3b0016d9fb7aec912b9e9958009e10bd
Marcin Rataj <lidel@lidel.org>
2018-11-19 15:53:51 +0100

    feat: detect and support window.ipfs.enable experiment (#2)
    This is a simple heuristic for forward-compatible detection of IPFS API exposed by ipfs-companion.
    
    While we are at it, make it possible to request specific permissions provided by this interface.
    
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- test.js
+++ test.js
@@ -12,6 +12,13 @@ test.serial('should use window.ipfs if available', async t => {
   t.is(ipfs, window.ipfs)
 })
 
+test.serial('should use window.ipfs.enable if available', async t => {
+  window.ipfs = { enable: async (args) => args }
+  const permissions = Object.freeze({ foo: ['a', 'b', 'c'], bar: ['1', '2', '3'], buzz: false })
+  const ipfs = await getIpfs({ permissions })
+  t.deepEqual(ipfs, permissions)
+})
+
 test.serial('should use window.Ipfs if available', async t => {
   const instance = {
     once (event, handler) {
